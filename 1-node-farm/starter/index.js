b5399931568d2b71013ab037ca7a815f62432580
ShengjieMao <56333697+ShengjieMao@users.noreply.github.com>
2024-05-14 22:29:35 -0700

    complete farm app

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,22 +1,29 @@
+// Core
 const fs = require('fs'); // require file system module
 const http = require('http'); // for server
 const url = require('url'); // routing
 
+// 3rd parties
+const slugify = require('slugify'); // having url more readable
+
+// User created
+const replaceTemplate = require('./modules/replaceTemplate');
+
 // -------------------------
-//FILES
-// synchroizing file reading, blocking way
+//--FILES
+//-- synchroizing file reading, blocking way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
-// // write to files
+//--  write to files
 // const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 // fs.writeFileSync("./txt/output.txt", textOut);
 // console.log("File written!");
 
-//asynchroizing file reading, non-blocking way
+//--asynchroizing file reading, non-blocking way
 // fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
 //   if (err) return console.log("ERROR");
 //   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
+//--     console.log(data2);
 //     fs.writeFile("./txt/final.txt", `${data2}`, "utf-8", (err) => {
 //       console.log("Your file has been written");
 //     });
@@ -26,23 +33,6 @@ const url = require('url'); // routing
 
 // -------------------------
 //SERVER
-const replaceTemplate = (temp, product) => {
-  // replace the product names globally
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-  return output;
-};
-
 // can use sync version since the json and template only need to load once
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -59,6 +49,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
